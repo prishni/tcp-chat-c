f9b41b368d69d03f7f3e4bea1b299095881ce840
prishni <prishni93rateria@gmail.com>
2017-01-25 17:23:21 +0530

    Add files via upload

---
+++ ass2_client.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>       
+#include <unistd.h>  	   //fork
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>  //read write
+#include <netdb.h> 	 //hostent structure
+#include <errno.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <poll.h>
+#include  <signal.h>
+#include <fcntl.h> /* Added for the nonblocking socket */
+#define BUF_SZ 500
+int sockfd;
+/*---------------------------------------------------------
+|	Function to handle ctrl+c (termination) of client	   |
+|	input- int                                             |
+|	output- void                                           |
+-----------------------------------------------------------*/
+void sighandler(int sig_num)
+{	
+		
+		printf("in sig handler\n");
+		char buffer[BUF_SZ];
+		printf("\r%c[2K",27);
+        sprintf(buffer, "%s", "-1");
+        int n = write(sockfd,buffer,BUF_SZ);
+		bzero(buffer,BUF_SZ);
+        exit(0);
+}
+int main(){
+
+	int portno = 5000;
+	int n;
+	char buffer[BUF_SZ];
+	char *input = NULL;
+	size_t size;
+	
+	sockfd = socket(AF_INET,SOCK_STREAM,0);
+	//fcntl(sockfd, F_SETFL, O_NONBLOCK);
+	int reuse = 1;
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT|SO_REUSEADDR, (const char*)&reuse, sizeof(reuse));
+	if(sockfd == -1) {printf("error:socket formation\n"); return 0;}
+	/*server information*/
+	struct hostent *server;	
+	server = gethostbyname("127.0.0.1");
+	struct sockaddr_in servaddr;
+	bzero((char *) &servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, (char *)&servaddr.sin_addr.s_addr, server->h_length);
+	servaddr.sin_port = htons(portno);
+	if(connect(sockfd,(struct sockaddr *) &servaddr,sizeof(servaddr))== -1) {perror("connect: "); return 0;}
+	
+	n = read(sockfd,buffer,BUF_SZ);
+	if(strcmp(buffer,"Connection Limited Exceeded")==0){
+		printf("%s\n", buffer);
+		exit(0);
+	}
+	printf("%s\n", buffer);
+	bzero(buffer,BUF_SZ);
+
+	struct pollfd fds[]={
+		{sockfd,POLLIN},
+		{0,POLLIN}
+	};
+	signal(SIGINT, sighandler);
+	printf("\r%c[2K",27);
+	while(1){
+		poll(fds,2,-1);
+		int i;
+		for(i=0;i<2;i++){
+			if(fds[i].revents & POLLIN ){ 
+				if(i==0){
+					//read server reply
+					n =read(sockfd,buffer,BUF_SZ);
+					printf("%s",buffer);
+					fflush(NULL);
+					bzero(buffer,BUF_SZ);
+				}
+				else if(i==1){
+					//take user input and send to the server
+					getline(&input,&size,stdin);
+					sprintf(buffer, "%s", input);
+					n = write(sockfd,buffer,BUF_SZ);
+					bzero(buffer,BUF_SZ);
+				}
+			}
+		}
+	}
+	
+
+	close(sockfd);
+	return 0;
+}
---
+++ ass2_server.c
@@ -0,0 +1,299 @@
+#include <stdio.h>
+#include <stdlib.h>        /*fork*/
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>    /*read, write, sockaddr*/
+#include <netdb.h> 	       /*To store server info in clinet's process, hostent struct*/
+#include <errno.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <time.h>
+#include <poll.h>
+#include  <signal.h>
+#include <fcntl.h>        /* Added for the nonblocking socket */
+#define BUF_SZ 500        /*size of buffer*/
+#define MAX_CLIENTS 5     /*max number of clients that can be connected*/
+
+/*---------------------------------------------------------
+|	structure to store all connected clients information  |
+-----------------------------------------------------------*/
+struct clientInfo{
+	int id;
+	char name[30];
+	int fd;
+	time_t timestamp;
+};
+/*----------------------------------------------------------
+|	structure to store messages       	                   |
+-----------------------------------------------------------*/
+struct message{
+	int s_id,r_id;
+	char s_name[30];
+	char r_name[30];
+	char msg[BUF_SZ];
+};
+/*---------------------------------------------------------
+|	generates a randon number in the range [0,RAND_MAx]	   |
+|	input- NULL                                            |
+|	output- int                                            |
+-----------------------------------------------------------*/
+int generateClientid(){
+	srand ( time(NULL) );
+  	int random_number = rand();
+  	return random_number;
+}
+/*---------------------------------------------------------
+|	generates a randon name for the client          	   |
+|	input- NULL                                            |
+|	output- char array                                     |
+-----------------------------------------------------------*/
+char * generateClientname(){
+
+}
+/*---------------------------------------------------------
+|	checks if there is message for client in yhe queue	   |
+|	input- msg queue,front pointer, client name(self)      |
+|	output- int                                            |
+-----------------------------------------------------------*/
+int checkQueue(struct message *msg_queue,int *front,char *myCliName){
+	struct message m = msg_queue[*front];
+	if(strcmp(m.r_name,myCliName)==0)
+		return 1;
+	return 0;
+}
+/*---------------------------------------------------------
+|	checks if the destination client exixts          	   |
+|	input- destination client's name, array of all         |
+|		   connected clients                               |
+|	output- int (1 if client exists, 0 otherwise)          |
+-----------------------------------------------------------*/
+int checkIfSrcExists(char *dest_name,int no_of_server,struct clientInfo clients[]){
+	for(int i=1;i<=no_of_server;i++){
+		if((strcmp(dest_name,clients[i].name)==0) && clients[i].id !=-1)
+			return 1;
+	}
+	return 0;
+}
+/*---------------------------------------------------------
+|	checks if the clients tries to ping itself        	   |
+|	input- destination client's name, source client's      |
+|		   name                                            |
+|	output- int (1 if client pings itself, 0 otherwise)    |
+-----------------------------------------------------------*/
+int ifMsgToItself(char *dest_name,char *src_name){
+	if(strcmp(dest_name,src_name)==0) return 1;
+	return 0;
+}
+/*---------------------------------------------------------
+|	dequeues message form queue      	                   |
+|	input- front & rear pointers, msg queue                |
+|	output- dequeued message                               |
+-----------------------------------------------------------*/
+struct message dequeue(int *front, int *rear, struct message *msg_queue){
+	struct message msg ;
+	if(!(*front > *rear || *front == 100 || *front == -1)){
+		msg = msg_queue[*front];
+		if(*front == *rear){
+			*front = *rear =-1;
+		}
+		else{
+			(*front)++;
+		}
+	}
+	return msg;
+}
+/*---------------------------------------------------------
+|	equeues a message     	                               |
+|	input- front & rear pointers, msg queue ,message  m    |
+|	output- void                                           |
+-----------------------------------------------------------*/
+void enqueue(int *front, int *rear,  struct message *msg_queue,struct message m){
+	if(*rear ==99) return;
+	else{
+		msg_queue[(*rear)+1] = m;
+		(*rear)++;
+		if(*front == -1) *front =0;
+	}
+}
+/*---------------------------------------------------
+|	Display all available users/clients to a client  |
+|	Input- NULL                                      |
+|	Output- client information                       |
+----------------------------------------------------*/
+void showUsers(struct clientInfo clients[],int no_of_server,int newsock_fd,int myid){
+	int n;
+	char buffer[BUF_SZ];
+	bzero(buffer,BUF_SZ);
+	char input[BUF_SZ];
+	int i,flag=0;
+	for(i=1;i<=no_of_server ;i++){
+		if(clients[i].id!=-1 && clients[i].id != myid){
+			sprintf(input,"  CLIENT%d:-\n  ID= %d\n  NAME= %s\n\n",i,clients[i].id,clients[i].name);
+			strcat(buffer,input);
+			bzero(input,BUF_SZ);
+			flag=1;
+		}
+	}
+	if(flag==0){
+		sprintf(buffer,"%s","NO OTHER CLIENTS CONNECTED\n");
+		n = write(newsock_fd,buffer,BUF_SZ);
+		bzero(buffer,BUF_SZ);
+	}
+	else{
+		n = write(newsock_fd,buffer,BUF_SZ);
+		bzero(buffer,BUF_SZ);
+	}
+}
+void broadcast(struct clientInfo clients[],int no_of_server,char *myCliName,int *front, int *rear,  struct message *msg_queue){
+	struct message m;
+	strcpy(m.s_name,myCliName);
+	strcpy(m.msg,"IS DISCONNECTED");
+	for(int i=1;i<=no_of_server;i++){
+		if(clients[i].id !=-1){
+			strcpy(m.r_name,clients[i].name);
+			enqueue(front,rear,msg_queue,m);
+		}
+	}
+}
+
+int main(){
+	
+	int port_no = 5000;
+	int sock_fd = socket(AF_INET,SOCK_STREAM,0);
+	if(sock_fd ==-1){printf("error:socket formation\n"); return 0;}
+	fcntl(sock_fd, F_SETFL, O_NONBLOCK);
+	/*server information*/
+	struct sockaddr_in servaddr;
+	bzero((char *)&servaddr,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr= INADDR_ANY;
+	servaddr.sin_port = htons(port_no);
+	if(bind(sock_fd,(struct sockaddr*)&servaddr,sizeof(servaddr)) <0);
+	listen(sock_fd,5);
+	
+	/*client information*/
+	struct sockaddr_in clientaddr;
+	socklen_t c_len = sizeof(clientaddr);
+	//int *no_of_server=0;
+
+	int shmid = shmget(IPC_PRIVATE,sizeof(int)*1, IPC_CREAT | 0666);
+	int *front =(int *)shmat(shmid,NULL,0);
+	shmid = shmget(IPC_PRIVATE,sizeof(int)*1, IPC_CREAT | 0666);
+	int *rear =(int *)shmat(shmid,NULL,0);
+	shmid = shmget(IPC_PRIVATE,sizeof(clientInfo)*5, IPC_CREAT | 0666);
+	struct clientInfo *clients =(struct clientInfo*)shmat(shmid,NULL,0);
+	shmid = shmget(IPC_PRIVATE,sizeof(message)*100, IPC_CREAT | 0666);
+	struct message *msg_queue =(struct message *)shmat(shmid,NULL,0);
+	shmid = shmget(IPC_PRIVATE,sizeof(int)*1, IPC_CREAT | 0666);
+	int *no_of_server =(int *)shmat(shmid,NULL,0);
+	*no_of_server=0;
+	*front =-1;
+	*rear =-1;
+	
+	for(;;)
+	{
+		int newsock_fd = accept(sock_fd,(struct sockaddr *)&clientaddr , &c_len );
+		if(newsock_fd ==-1){continue;}
+		fcntl(newsock_fd, F_SETFL, O_NONBLOCK);
+		char buffer[BUF_SZ];
+		struct pollfd fds[]={
+			{newsock_fd,POLLIN},
+			{0,POLLIN}
+		};
+
+		/* If 5 clients are already connected than prohibit further connections*/
+		if(*no_of_server >= MAX_CLIENTS){
+			sprintf(buffer, "%s","CONNECTION LIMIT EXCEEDED\n");
+			int n = write(newsock_fd,buffer,BUF_SZ);
+			bzero(buffer,BUF_SZ);
+			close(newsock_fd);
+		}
+		(*no_of_server)++;
+		
+		pid_t pid =fork();
+		if(pid == -1){
+			close(newsock_fd);
+			continue;
+		}
+		if(pid > 0){							//Parent
+			close(newsock_fd);
+			continue;
+		}
+		if(pid == 0){							//Child
+			int n;
+			int myclient = *no_of_server;
+			char myCliName[30] ="client";
+			clients[*no_of_server].id=generateClientid()%100;
+			char temp[5];
+			sprintf(temp,"%d",myclient);
+			strcat(myCliName,temp);
+			strcpy(clients[*no_of_server].name,myCliName);
+			clients[*no_of_server].fd=newsock_fd;
+			clients[*no_of_server].timestamp= time(NULL);
+			sprintf(buffer,"\n WELCOME, YOU ARE CONNECTED \n ID = %d\n NAME = %s \n TIME STAMP = %s ",clients[*no_of_server].id,clients[*no_of_server].name ,ctime(&clients[*no_of_server].timestamp));
+			n = write(newsock_fd,buffer,BUF_SZ);
+			bzero(buffer,BUF_SZ);
+			printf("A client connected with\n Id = %d\n Name = %s \n TimeStamp = %s\n",clients[*no_of_server].id,clients[*no_of_server].name ,ctime(&clients[*no_of_server].timestamp));
+			//showUsers(clients,*no_of_server,newsock_fd,clients[myclient].id);
+			while(1){
+					n = read(newsock_fd,buffer,BUF_SZ);
+					if(n>0){
+						if(strncmp("showUsers\n",buffer,BUF_SZ)==0){
+							bzero(buffer,BUF_SZ);
+							showUsers(clients,*no_of_server,newsock_fd,clients[myclient].id);
+						}
+						else if(strncmp("-1",buffer,BUF_SZ)==0){
+							printf("%s with id %d is disconnected\n\n",myCliName,clients[myclient].id);
+							clients[myclient].id =-1;
+							bzero(buffer,BUF_SZ);
+							broadcast(clients,*no_of_server,myCliName,front,rear,msg_queue);
+							close(sock_fd);
+							return 0;
+						}
+						else{
+							int index= strcspn(buffer,":");
+							struct message m;
+							char *src_name = myCliName;
+							char dest_name[30],msg[BUF_SZ];
+							memcpy(dest_name,&buffer,index);
+							sprintf(msg,"%s",buffer+index+1);
+							msg[strlen(msg)-1]='\0';
+							dest_name[index]='\0';
+							strcpy(m.msg,msg);
+							strcpy(m.s_name,src_name);
+							strcpy(m.r_name,dest_name);
+							m.s_id=-1;
+							m.r_id=-1;
+							
+							if(ifMsgToItself(dest_name,src_name)){
+								sprintf(buffer,"%s","YOU CANNOT PING YOURSELF\n");
+								write(newsock_fd,buffer,BUF_SZ);
+								bzero(buffer,BUF_SZ);
+							}
+							else if(checkIfSrcExists(dest_name,*no_of_server,clients)){
+								enqueue(front,rear,msg_queue,m);
+							}
+							else{
+								sprintf(buffer,"%s","THIS CLIENT DOESNOT EXISTS\n");
+								write(newsock_fd,buffer,BUF_SZ);
+								bzero(buffer,BUF_SZ);
+							}
+						}
+					}
+					else {
+						if(checkQueue(msg_queue,front,myCliName)){
+							struct message m =dequeue(front,rear,msg_queue);
+							sprintf(buffer,"%s: \"%s\" \n",m.s_name, m.msg);
+							write(newsock_fd,buffer,BUF_SZ);
+							bzero(buffer,BUF_SZ);
+						}
+					}
+			}
+			break;
+		}
+	}
+	close(sock_fd);
+	return 0;
+}
